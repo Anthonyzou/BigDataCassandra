5ee1ceca0d78f2920609c6697d2ad8b51c4a053f
Anthony Ou <azou@ualberta.ca>
2014-02-12 16:11:46 -0700

    fixed stuff and java folder

--- pythonClient/src/client/client.py
+++ pythonClient/src/client/client.py
@@ -3,7 +3,7 @@ from cassandra.cluster import Cluster
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect('cmput391') #keyspace
 
-rows = session.execute('SELECT * from call_details_record')
+rows = session.execute('SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies')
 
 for row in rows:
     print row
--- rubyClient/client.rb
+++ rubyClient/client.rb
@@ -2,12 +2,14 @@ class Client
   require 'cql'
   
   client = Cql::Client.connect(hosts: ['127.0.0.1'])
-  client.use('cmput391')
-  rows = client.execute('SELECT * FROM call_details_record')
-    rows.each do |row|
-      puts row
-#  rows = client.execute('SELECT keyspace_name, columnfamily_name FROM call_details_records')
+  client.use('system')
+#  rows = client.execute('SELECT * FROM call_details_record')
 #  rows.each do |row|
-#    puts "The keyspace #{row['keyspace_name']} has a table called #{row['columnfamily_name']}"
+#    puts row
+#  end
+  
+  rows = client.execute('SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies')
+  rows.each do |row|
+    puts "The keyspace #{row['keyspace_name']} has a table called #{row['columnfamily_name']}"
   end
 end
