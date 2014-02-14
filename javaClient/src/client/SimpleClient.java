55e91c0d9517e56147a5448caf3cfcca8c491b3a
Anthony Ou <azou@ualberta.ca>
2014-02-14 03:34:01 -0700

    javaclient now does the same thing as the rest of them

--- javaClient/src/client/SimpleClient.java
+++ javaClient/src/client/SimpleClient.java
@@ -3,20 +3,16 @@ package client;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
 
 public class SimpleClient {
 	private Cluster cluster;
 
-	public void connect(String node) {
-		cluster = Cluster.builder()
-				.addContactPoint(node).build();
-		Metadata metadata = cluster.getMetadata();
-		System.out.printf("Connected to cluster: %s\n", 
-				metadata.getClusterName());
-		for ( Host host : metadata.getAllHosts() ) {
-			System.out.printf("Datatacenter: %s; Host: %s; Rack: %s\n",
-					host.getDatacenter(), host.getAddress(), host.getRack());
-		}
+	public Session connect(String node) {
+		cluster = Cluster.builder().addContactPoint(node).build();
+		return cluster.connect("system");	
 	}
 
 	public void close() {
@@ -25,7 +21,13 @@ public class SimpleClient {
 
 	public static void main(String[] args) {
 		SimpleClient client = new SimpleClient();
-		client.connect("127.0.0.1");
+		Session s = client.connect("127.0.0.1");
+		s.getCluster();
+		ResultSet rs = s.execute("SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies");
+		for(Row r : rs){
+			System.out.println(r);
+		}
 		client.close();
+		
 	}
 }
