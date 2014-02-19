1d04ffc34dfd474a659a87b93094d582fedcde55
anthony ou <azou@ualberta.ca>
2014-02-19 00:30:42 -0700

    restoring files

---
+++ javaClient/src/client/SimpleClient.java
@@ -0,0 +1,36 @@
+package client;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class SimpleClient {
+	private Cluster cluster;
+
+	public Session connect(InetAddress ... node) {
+		cluster = Cluster.builder()
+						.addContactPoints(node)
+						.build();
+		return cluster.connect("system");	
+	}
+
+	public void close() {
+		cluster.shutdown();
+	}
+
+	public static void main(String[] args) throws UnknownHostException {
+		SimpleClient client = new SimpleClient();
+		Session s = client.connect(InetAddress.getByAddress("uofa391-instance-2", new byte[] {10,0,0,38}));
+		s.getCluster();
+		ResultSet rs = s.execute("SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies");
+		for(Row r : rs){
+			System.out.println(r);
+		}
+		client.close();
+		
+	}
+}
