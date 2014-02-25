f17375bd43197cfbeaf53b46093e3a52a52e9c1e
anthony ou <azou@ualberta.ca>
2014-02-24 17:27:10 -0700

    example insert statements

--- pythonClient/client.py
+++ pythonClient/client.py
@@ -4,10 +4,23 @@ cluster = Cluster( ['10.0.0.31',
                     '10.0.0.38',
                     '127.0.0.1'
                     ])
-session = cluster.connect('system') #keyspace
+session = cluster.connect('system') #keyspace should be our own
+
+
+# query = "INSERT INTO users (id, name, age) VALUES (?, ?, ?)"
+# prepared = session.prepare(query)
+# session.execute(prepared, (user.id, user.name, user.age))
+# 
+# prepared = session.prepare(query)
+# for user in users:
+#      bound = prepared.bind((user.id, user.name, user.age))
+#      session.execute(bound)
+#      
+# prepared = session.prepare(query)
+# bound_stmt = prepared.bind((user.id, user.name, user.age))
+# session.execute(bound_stmt)
 
 rows = session.execute('SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies')
-
 for row in rows:
     print row
     
