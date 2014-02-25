72222afe814c541e9f8bedac5179cc83f7100c40
anthonyzou <azou@ualberta.ca>
2014-02-25 16:57:23 -0700

    Random ints in Python

--- pythonClient/client.py
+++ pythonClient/client.py
@@ -1,10 +1,17 @@
 from cassandra.cluster import Cluster
-
+import random
+from random import randint
+from xmlrpclib import MAXINT, MININT
+ 
 cluster = Cluster( ['10.0.0.31',
                     '10.0.0.38',
                     '127.0.0.1'
                     ])
-session = cluster.connect('system') #keyspace should be our own
+#session = cluster.connect('system') #keyspace should be our own
+
+random.seed(3333)
+print randint(MININT, MAXINT)
+print randint(MININT, MAXINT)
 
 
 # query = "INSERT INTO users (id, name, age) VALUES (?, ?, ?)"
