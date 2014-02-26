3ca30c99fad514a3fdc9da6b59d69af68017c8c1
anthonyzou <azou@ualberta.ca>
2014-02-26 13:49:29 -0700

    added random table generation

--- pythonClient/client.py
+++ pythonClient/client.py
@@ -9,24 +9,33 @@ cluster = Cluster( ['199.116.235.57',
                     '127.0.0.1'
                     ])
 
-session = cluster.connect('system') #keyspace should be our own
+session = cluster.connect('group3') #keyspace should be our own
 
 metadata = cluster.metadata
 print metadata.cluster_name #should make sure this is group3
 
 random.seed(3333)
 
+query = "drop table users"
+session.execute(query)
+query = """CREATE TABLE users (
+           id int PRIMARY KEY,
+           name int,
+           age int)"""
+session.execute(query)
+
+
 query = "INSERT INTO users (id, name, age) VALUES (?, ?, ?)" 
 prepared = session.prepare(query)
-
+ 
 #example async insert into table.
-for x in range(0,100):
-    bound = None
-    for y in range(0, 3):
-        if randint(0,1) == 1:
-            bound = prepared.bind((None))
-        else: 
-            bound = prepared.bind((randint(1,2)))
+
+
+for y in range(10):
+    build = []
+    for x in range(3):
+        build.append((randint(1,MAXINT)))
+    bound = (prepared.bind(build) )
     session.execute_async(bound)
 
     
