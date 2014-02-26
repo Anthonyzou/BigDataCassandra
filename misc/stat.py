9b4bc85d75302366b7171ed25cbf954292a713e4
Shen Wei Liao <shenweiliao@gmail.com>
2014-02-26 16:22:15 -0700

    Insert table working. Population not.

--- misc/stat.py
+++ misc/stat.py
@@ -1,21 +1,27 @@
 # Finds how many entries are filled per 1000 columns in the table.
 
+import pylab as pl
 # Get information from table data and labels.
 # I dunno why there is one less column than label though...
 with open("partial_data_table1.txt") as pdt:
     stuff = [tuple(line.strip().split(";")) for line in pdt]
 
 with open("bigdata_setup1.sql") as labelfile:
-    labels = [line.strip() for line in labelfile]
-labels = labels[5:-1]
+    labels = [line.strip().rstrip(',') for line in labelfile]
+labels2 = labels[5:-1]
+labels = [label.split() for label in labels2]
 
-# count instances of each column entry
-counter = [0] * len(stuff[0])
+# get counts of each column and plot the count
+count = [0] * len(stuff[0])
 for line in stuff:
     for index, element in enumerate(line):
         if element:
-            counter[index] += 1
+            count[index] += 1
+labels, count = zip(*sorted(zip(labels, count), key=lambda e: -e[1]))
 
-# print frequency (out of 1000) for each label
-for i in range(len(labels)):
-    print(labels[i] + str(counter[i]))
+print labels2
+print count
+
+# plot stuffs
+#pl.bar(range(len(count)), count, label=labels)
+#pl.show()
