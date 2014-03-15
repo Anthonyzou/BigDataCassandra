da2913cea2ec64f259cfb349fc655d0bb5c5b8c3
Anthony Ou <azou@ualberta.ca>
2014-03-14 18:57:48 -0600

    tokens being used and query.py updated to record time

--- pythonClient/query.py
+++ pythonClient/query.py
@@ -8,9 +8,6 @@ import cassandra
 from cassandra.cluster import Cluster
 
 
-
-start_time = time.clock()
-
 cluster = Cluster(['199.116.235.57', '10.0.0.31', '10.0.0.38', '127.0.0.1'])
 
 
@@ -19,27 +16,31 @@ print cluster.metadata.cluster_name  # should make sure this is group3
 print cassandra.__version__
 
 # QUERY 1
-
+start_time = time.clock()
 query = ""
 session.execute(query)
+print str((time.clock() - start_time) / 60)[:7], "minutes elapsed"
 
 # QUERY 2
-
+start_time = time.clock()
 query = ""
 session.execute(query)
+print str((time.clock() - start_time) / 60)[:7], "minutes elapsed"
 
 # QUERY 3
-
+start_time = time.clock()
 query = ""
 session.execute(query)
+print str((time.clock() - start_time) / 60)[:7], "minutes elapsed"
 
 # QUERY 4
-
+start_time = time.clock()
 query = ""
 session.execute(query)
+print str((time.clock() - start_time) / 60)[:7], "minutes elapsed"
 
 # QUERY 5
-
+start_time = time.clock()
 query = ""
 session.execute(query)
-
+print str((time.clock() - start_time) / 60)[:7], "minutes elapsed"
