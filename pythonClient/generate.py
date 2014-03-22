1213ed79d476d8706767ee722e0ff683c6d780ee
Anthony Ou <azou@ualberta.ca>
2014-03-22 13:42:52 -0600

    added two tables

--- pythonClient/generate.py
+++ pythonClient/generate.py
@@ -5,28 +5,31 @@ from datetime import date
 import random
 import string
 import timeit
-
-acluster = 0
+import datetime
+from _socket import timeout
 
 
 def randword(length):
     return ''.join(random.choice(string.lowercase) for i in range(length))
 
+acluster = 0
+datadate = 1385305327 #equal to 2013-11-24 08:02:07-0700
 session = None
 def generate(label, element_type, frequency):
     global acluster
     global session
+    global datadate
     """ Generate an element value to insert. of arbitrary type, which is a null
     value (1000-frequency)/1000 of the time
     """
     if (random.randint(0, 1000) < frequency):
         # column-specific data
-        if (label == "MSC_CODE"):  # pretend this is partition by cluster
+        if (label == "MOBILE_ID_TYPE"):  # pretend this is partition by cluster
             result = acluster % 8
             acluster += 1
         elif (label == "MONTH_DAY"):
             result = random.randint(1, 31)
-            session.execute_async(session.prepare("update group_by_month set count = count + 1  where MONTH_DAY = ?").bind([result]))
+            session.execute(session.prepare("update group_by_month set count = count + 1  where MONTH_DAY = ?").bind([result]),timeout=None)
         elif (label == "LONGITUDE" or label == "LAST_LONGITUDE"):
             result = random.random() * 360 - 180
         elif (label == "LATITUDE" or label == "LAST_LATITUDE"):
@@ -37,9 +40,9 @@ def generate(label, element_type, frequency):
         elif (element_type == "text"):
             result = randword(16)
         elif (element_type == "timestamp"):
-            result = random.randint(1385305327, 1395305327)
+            result = datadate
         else:
-            result = random.randint(0, 1000000)
+            result = random.randint(0, 9900000)
     else:
         result = None
     return result
@@ -58,28 +61,21 @@ if __name__ == '__main__':
     random.seed(3333)
     
     session.execute("drop table IF EXISTS cdr")
+    session.execute("drop table IF EXISTS query3")
     session.execute("drop table IF EXISTS group_by_month")
 
     try:
         with open("cdr_table.sql") as tables_setup:
             cols = tables_setup.read()
             session.execute(
-                "CREATE TABLE cdr(" + cols + """primary key(MSC_CODE ,CITY_ID,
-                PCMD_VER  ,
-                SEQ_NUM ,
-                MONTH_DAY ,
-                DUP_SEQ_NUM ,
-                MOBILE_ID_TYPE ,
-                SESS_REQ_TYPE ,
-                SESS_SFC ,
-                SESS_OR_CONN_CPFAIL ,
-                CFC)) with clustering order by (city_id asc)""")
+                "CREATE TABLE cdr(" + cols + """primary key(MSC_CODE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
+                MONTH_DAY ,DUP_SEQ_NUM ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
+                CON_OHM_NUM)) with clustering order by (city_id asc)""")
+            session.execute(
+                "CREATE TABLE query3(" + cols + """primary key(MSC_CODE ,MOBILE_ID_TYPE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
+                MONTH_DAY ,DUP_SEQ_NUM ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
+                CON_OHM_NUM)) with clustering order by (MOBILE_ID_TYPE asc)""")
             session.execute("Create table group_by_month (MONTH_DAY int, count counter, primary key (month_day))")
-            session.execute("CREATE INDEX ON cdr (MOBILE_ID_TYPE)")
-            session.execute("CREATE INDEX ON cdr (LONGITUDE)")
-            session.execute("CREATE INDEX ON cdr (LATITUDE)")
-            session.execute("CREATE INDEX ON cdr (LAST_LONGITUDE)")
-            session.execute("CREATE INDEX ON cdr (LAST_LATITUDE)")
             
     except Exception as error:
         print error
@@ -93,26 +89,31 @@ if __name__ == '__main__':
     counts = ast.literal_eval(counts)  # turn input into list correctly
     types = []
 
-    query = "INSERT INTO cdr ("
+    body = ""
     # build columns
     for i in labels:
-        query += i[0] + ","
+        body += i[0] + ","
         types.append(i[1])
-
+    body = body[:-1] + ") VALUES (" + ("?," * (len(labels) - 1)) + "?)"
     # remove last char
     # build question marks for binding
-    prepared = session.prepare(
-        query[:-1] + ") VALUES (" + ("?," * (len(labels) - 1)) + "?)")
+    prepared = session.prepare("INSERT INTO cdr ("+ body )
+    prepared2 = session.prepare("INSERT INTO query3 (" + body)
     print "query built and prepared"
 
     # example async insert into table
-    for y in range(3000):
-        build = []
-        for x in range(len(labels)):
-            #print("counts: " + str(counts[x]))
-            thing = generate(labels[x][0], types[x], counts[x])
-            build.append(thing)
-        bound = prepared.bind(build)
-        session.execute_async(bound)
+    try:
+        for day in range(1):
+            for entry in range(150):
+                if entry == 150:
+                    datadate += 86400
+                build = []
+                for x in range(len(labels)):
+                    #print("counts: " + str(counts[x]))
+                    build.append(generate(labels[x][0], types[x], counts[x]))
+                session.execute_async( prepared.bind(build))
+                session.execute_async( prepared2.bind(build))
+    except Exception as error:
+        print entry, error
 
     print str((timeit.default_timer() - start_time)/60)[:7], "seconds elapsed"
