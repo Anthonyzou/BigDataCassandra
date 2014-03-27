1d033b3a059ea6843fe05bd1563526a5dea11812
Anthony Ou <azou@ualberta.ca>
2014-03-27 15:33:14 -0600

    put in a setup command in shell

--- group3.sh
+++ group3.sh
@@ -1,33 +1,24 @@
 #!/bin/sh
-echo
-echo
-echo 'generate test data locally : 	1'
-echo 'generate data remotely : 	2 for development only'
-echo '	query locally : 	3'
-echo '	query remotely : 	4 for development only'
-echo '		status :	5'
-echo '  ssh into instance 1 :		6'
-echo '	 	make zip :	7'
-echo 'generate test data optimized : 8'
-echo 'generate 16TB locally : 9'
-echo 'generate 16TB locally optmized : 10'
+echo '\n\n'
+echo '	generate test data locally : 		1'
+echo '	generate data remotely : 		2 for development only'
+echo '	query locally : 			3'
+echo '	query remotely : 			4 for development only'
+echo '	status :				5'
+echo '	ssh into instance 1 :			6'
+echo '	make zip :				7'
+echo '	generate 16TB locally :			8'
+echo '	setup this machine :			9'
+echo '\n\n'
 
 generate(){
-	nohup python -W ignore generate.py 1 > misc/generation_data.txt &
+	echo '		enter days to generate!'
+	read amount
+	nohup python -W ignore generate.py "$amount" > misc/generation_data.txt &
 }
-
-generate_optimized(){
-	nohup python -W ignore generate.py 1 true > misc/generation_data.txt &
-}
-
 generate_big(){
 	nohup python -W ignore generate.py 10000 > misc/generation_data.txt &
 }
-
-generate_big_optimized(){
-	nohup python -W ignore generate.py 10000 true > misc/generation_data.txt &
-}
-
 remote_generate(){
 	echo
 	echo 'default group3@199.116.235.57'
@@ -58,6 +49,14 @@ zip(){
 	tar -cv docs query.py misc generate.py cdr_table.sql tablestuffs.txt NebulaLaunchKey.pem README.md | gzip -c > project.tgz
 }
 
+setup(){
+	curl http://www.apache.org/dyn/closer.cgi?path=/cassandra/2.0.6/apache-cassandra-2.0.6-bin.tar.gz | tar xf C $HOME
+	export PATH=$PATH:$HOME/apache-cassandra-2.0.6/bin
+	echo '	ENTER THIS MACHINES IP'
+	read input
+	sed "s/10.0.0.31/$input/g" misc/cassandra.yaml >> $HOME/apache-cassandra-2.0.6/conf/cassandra.yaml
+}
+
 chmod 600 NebulaLaunchKey.pem
 read input
 case $input in
@@ -68,9 +67,8 @@ case $input in
 	5) status;;
 	6) instance1;;
 	7) zip;;
-	8) generate_optimized;;
-	9) generate_big;;
-	10) generate_big_optimized;;
+	8) generate_big;;
+	9) setup;;
 
-	*) echo "invalid";;
+	*) echo "exiting";;
 esac
