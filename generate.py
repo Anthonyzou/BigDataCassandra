6fbb834b9e427934440ec0955b0ff820480d5913
Anthony Ou <azou@ualberta.ca>
2014-04-04 21:47:36 -0600

    better server config and no nulls

--- generate.py
+++ generate.py
@@ -19,36 +19,33 @@ def generate(label, element_type, frequency):
     """ Generate an element value to insert. of arbitrary type, which is a null
     value (1000-frequency)/1000 of the time
     """
-    if (random.randint(0, 1000) <= frequency):
-        # column-specific data
-        if (label == "MOBILE_ID_TYPE"):  # pretend this is partition by cluster
-            result = acluster % 8
-            session.execute_async(
-                    session.prepare("update group_by_MOBILE_ID_TYPE set count = count + 1  where MOBILE_ID_TYPE = ? and id = 1")
-                    .bind([result]))
-            acluster += 1
-        elif label == "SEIZ_CELL_NUM_L":
-            result = uuid.uuid4()
-        elif (label == "MONTH_DAY"):
-            result = random.randint(1, 31)
-            session.execute_async(
-                    session.prepare("update group_by_month set count = count + 1  where MONTH_DAY = ? and id = 1")
-                    .bind([result]))
-        elif (label == "LONGITUDE" or label == "LAST_LONGITUDE"):
-            result = random.random() * 360 - 180
-        elif (label == "LATITUDE" or label == "LAST_LATITUDE"):
-            result = random.random() * 180 - 90
-        # type-specific fallback data
-        elif (element_type == "float"):
-            result = random.random()
-        elif (element_type == "text"):
-            result = randword(16)
-        elif (element_type == "timestamp"):
-            result = datadate
-        else:
-            result = random.randint(0, 9900000)
+    # column-specific data
+    if (label == "MOBILE_ID_TYPE"):  # pretend this is partition by cluster
+        result = acluster % 8
+        session.execute_async(
+                session.prepare("update group_by_MOBILE_ID_TYPE set count = count + 1  where MOBILE_ID_TYPE = ? and id = 1")
+                .bind([result]))
+        acluster += 1
+    elif label == "SEIZ_CELL_NUM_L":
+        result = uuid.uuid4()
+    elif (label == "MONTH_DAY"):
+        result = random.randint(1, 31)
+        session.execute_async(
+                session.prepare("update group_by_month set count = count + 1  where MONTH_DAY = ? and id = 1")
+                .bind([result]))
+    elif (label == "LONGITUDE" or label == "LAST_LONGITUDE"):
+        result = random.random() * 360 - 180
+    elif (label == "LATITUDE" or label == "LAST_LATITUDE"):
+        result = random.random() * 180 - 90
+    # type-specific fallback data
+    elif (element_type == "float"):
+        result = random.random()
+    elif (element_type == "text"):
+        result = randword(16)
+    elif (element_type == "timestamp"):
+        result = datadate
     else:
-        result = None
+        result = random.randint(0, 9900000)
     return result
 
 
@@ -66,17 +63,20 @@ if __name__ == '__main__':
         session.execute("use group3")
     except: 
         session.execute("drop keyspace if exists group3", timeout=None)
-        session.execute("CREATE KEYSPACE group3 WITH REPLICATION = { 'class' : 'SimpleStrategy','replication_factor' : 1 }",timeout=None)
+        session.execute("""CREATE KEYSPACE group3 WITH REPLICATION = { 'class' : 'SimpleStrategy','replication_factor' : 3 } 
+                            AND durable_writes = false""",timeout=None)
         session.execute("use group3",timeout=None)
         random.seed(seed)
         with open("tableColumns.sql") as tables_setup:
             cols = tables_setup.read()
             for setupcmd in ["CREATE TABLE cdr(" + cols + """primary key(MSC_CODE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
                                 MONTH_DAY ,DUP_SEQ_NUM ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc) and compression={ 'sstable_compression':''}"""
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc) 
+                                and  compaction={'class': 'LeveledCompactionStrategy'} and compression={ 'sstable_compression':''} """
                              ,"CREATE TABLE query3(" + cols + """primary key(MSC_CODE ,DUP_SEQ_NUM ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
                                 MONTH_DAY ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (DUP_SEQ_NUM asc) and compression={ 'sstable_compression':''}"""
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (DUP_SEQ_NUM asc) 
+                                and  compaction={'class': 'LeveledCompactionStrategy'} and compression={ 'sstable_compression':''} """
                               ,"Create table group_by_month (id int, MONTH_DAY int, count counter, primary key (id,month_day)) with clustering order by (month_day asc)"
                               ,"Create table group_by_MOBILE_ID_TYPE (id int,MOBILE_ID_TYPE int, count counter, primary key (id,MOBILE_ID_TYPE))with clustering order by (mobile_id_type asc)"
                               , "create index on cdr (month_day)", "create index on cdr (MOBILE_ID_TYPE)"]:
