a55402b6f96320e064c406fb3cad64d43ae541e7
Anthony Ou <azou@ualberta.ca>
2014-04-04 20:50:06 -0600

    tables compression off by default

--- generate.py
+++ generate.py
@@ -73,10 +73,10 @@ if __name__ == '__main__':
             cols = tables_setup.read()
             for setupcmd in ["CREATE TABLE cdr(" + cols + """primary key(MSC_CODE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
                                 MONTH_DAY ,DUP_SEQ_NUM ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc)"""
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc) and compression={ 'sstable_compression':''}"""
                              ,"CREATE TABLE query3(" + cols + """primary key(MSC_CODE ,DUP_SEQ_NUM ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
                                 MONTH_DAY ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (DUP_SEQ_NUM asc)"""
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (DUP_SEQ_NUM asc) and compression={ 'sstable_compression':''}"""
                               ,"Create table group_by_month (id int, MONTH_DAY int, count counter, primary key (id,month_day)) with clustering order by (month_day asc)"
                               ,"Create table group_by_MOBILE_ID_TYPE (id int,MOBILE_ID_TYPE int, count counter, primary key (id,MOBILE_ID_TYPE))with clustering order by (mobile_id_type asc)"
                               , "create index on cdr (month_day)", "create index on cdr (MOBILE_ID_TYPE)"]:
@@ -105,7 +105,7 @@ if __name__ == '__main__':
     print( "query built and prepared")
     
     days = 1
-    entriesPerDay = 1000000
+    entriesPerDay = 100000
     try:
         if int(sys.argv[1]) >= 1:
             days = int(sys.argv[1])
