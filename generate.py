01089d4c5d38bb9570f95a486f809119376967fe
Anthony Ou <azou@ualberta.ca>
2014-04-08 12:12:43 -0600

    updated csv format

--- generate.py
+++ generate.py
@@ -14,9 +14,7 @@ acluster = 0
 datadate = 1385305327 #equal to 2013-11-24 08:02:07-0700
 session = None
 def generate(label, element_type, frequency):
-    global acluster
-    global session
-    global datadate
+    global acluster, session, datadate
     """ Generate an element value to insert. of arbitrary type, which is a null
     value (1000-frequency)/1000 of the time
     """
@@ -49,7 +47,7 @@ def generate(label, element_type, frequency):
         elif (element_type == "timestamp"):
             result = datadate
         else:
-            result = random.randint(-4000000000, 4000000000)
+            result = random.randint(0, 4000000000)
     else:
         result = ""
     return result
@@ -76,10 +74,12 @@ if __name__ == '__main__':
             cols = tables_setup.read()
             for setupcmd in ["CREATE TABLE cdr(" + cols + """primary key(MSC_CODE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
                                 MONTH_DAY ,DUP_SEQ_NUM ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc)"""
-                             ,"CREATE TABLE query3(" + cols + """primary key(MSC_CODE ,DUP_SEQ_NUM ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
-                                MONTH_DAY ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
-                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (DUP_SEQ_NUM asc)"""
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (city_id asc) and compression={ 'sstable_compression':''}"""
+                             ,"CREATE TABLE query3(" + cols + """primary key(MSC_CODE ,CITY_ID,SERVICE_NODE_ID,RUM_DATA_NUM ,
+                                MONTH_DAY ,DUP_SEQ_NUM ,MOBILE_ID_TYPE ,SEIZ_CELL_NUM ,FLOW_DATA_INC ,SUB_HOME_INT_PRI ,
+                                CON_OHM_NUM,SEIZ_CELL_NUM_L)) with clustering order by (CITY_ID asc,SERVICE_NODE_ID asc,RUM_DATA_NUM asc,
+                                MONTH_DAY asc,DUP_SEQ_NUM asc,MOBILE_ID_TYPE asc,SEIZ_CELL_NUM asc,FLOW_DATA_INC asc,SUB_HOME_INT_PRI asc,
+                                CON_OHM_NUM asc,SEIZ_CELL_NUM_L asc) and compression={ 'sstable_compression':''}"""
                               ,"Create table group_by_month (id int, MONTH_DAY int, count counter, primary key (id,month_day)) with clustering order by (month_day asc)"
                               ,"Create table group_by_MOBILE_ID_TYPE (id int,MOBILE_ID_TYPE int, count counter, primary key (id,MOBILE_ID_TYPE))with clustering order by (mobile_id_type asc)"
                               , "create index on cdr (month_day)", "create index on cdr (MOBILE_ID_TYPE)"]:
@@ -91,17 +91,17 @@ if __name__ == '__main__':
     with open("tableFrequency.txt") as tables_freq:
         (labels) = tables_freq.readlines()[0]
     labels = ast.literal_eval(labels)  # turn input into list correctly
-    days = 1
+   
     entriesPerDay = 10000
-    try:
-        if int(sys.argv[1]) >= 1:
-            days = int(sys.argv[1])
-    except:
-        pass
+    try: days = int(sys.argv[1])
+    except: days = 1
     cdr = subprocess.Popen(["/$HOME/Documents/apache-cassandra-2.0.6/bin/cqlsh "+" localhost "+ " 9133 "+" -k "+" group3"],
                         stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
     cdr.stdin.write("COPY cdr FROM STDIN;\n")
     
+    query3 = subprocess.Popen(["/$HOME/Documents/apache-cassandra-2.0.6/bin/cqlsh "+" localhost "+ " 9133 "+" -k "+" group3"],
+                        stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell=True)
+    query3.stdin.write("COPY query3 FROM STDIN;\n")
                 #datadate += 86400 #increment one day
     for i in range(days):
         for k in range(entriesPerDay):
@@ -112,10 +112,14 @@ if __name__ == '__main__':
                     gen += ","
             cdr.stdin.write(gen)
             cdr.stdin.write("\n")
+            query3.stdin.write(gen)
+            query3.stdin.write("\n")
     cdr.stdin.write("\.\n")
     cdr.stdin.close()
     cdr.wait()
-
+    query3.stdin.write("\.\n")
+    query3.stdin.close()
+    query3.wait()
  
     print str((timeit.default_timer() - start_time)/60), " minutes elapsed"
     print seed, "seed used", days, 'days generated'
